3226a225190f9497f78210137812c5c6c2fc9e33
Nitin shukla <nitin.shukla12004@gmail.com>
2024-06-01 14:58:46 +0530

    added js

--- script.js
+++ script.js
@@ -1,20 +1,45 @@
-document.addEventListener("click",function(event){
-    // elementr = event.target
-    // text = elementr.textContent
-    // newdiv = document.createElement('span')
-    // newdiv.innerHTML = text
-    // document.querySelector('.displayarea').appendChild(newdiv)
-    // s
 
+flag =0
+
+string = ""
+
+isequalpressed =false
+
+document.addEventListener("click",function(event){
+    if(isequalpressed== true){
+        document.querySelector('.displayarea').replaceChildren()
+        isequalpressed=false
+    }
     elementr = event.target
     text = elementr.textContent
-    while(text!='='){
-        newdiv = document.createElement('span')
+    if(flag==1 && text!="AC"){
+        document.querySelector('.displayarea').replaceChildren()
+        document.querySelector('.displayarea').style.visibility= "visible"
+        flag=0
+    }
+    newdiv = document.createElement('span')
+    document.querySelector('.displayarea').appendChild(newdiv)
+    if(text!="="){
         newdiv.innerHTML = text
-        document.querySelector('.displayarea').appendChild(newdiv)
-        string=""
+    }
+    newdiv.style.textAlign= "center"
+    if(text=="AC" && flag ==0){
+        document.querySelector('.displayarea').style.visibility= "hidden"
+        flag=1
+    }
+    if(text=="="){
+        document.querySelector('.displayarea').replaceChildren()
+        ans= document.createElement('span')
+        ans.innerHTML= eval(string)
+        document.querySelector('.displayarea').appendChild(ans)
+        isequalpressed= true
+        string =""
+    }
+    if(text!="AC" && text!="="){
         string+=text
     }
-    seldiv= document.getElementsByTagName('span')
-    seldiv.innerHTML= eval(string)
+    console.log(string)
+    
+    
 })
+
