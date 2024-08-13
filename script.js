aee1a9dc1c1cd6bf56c5b16b87405211ca0e0323
Nitin shukla <nitin.shukla12004@gmail.com>
2024-08-13 15:58:12 +0530

    prettified the javascript code

--- script.js
+++ script.js
@@ -1,61 +1,52 @@
+flag = 0;
 
-flag =0
+string = "";
 
-string = ""
+isequalpressed = false;
 
-isequalpressed =false
+document.addEventListener("click", function (event) {
+  if (isequalpressed == true) {
+    document.querySelector(".displayarea").replaceChildren();
+    isequalpressed = false;
+  }
+  elementr = event.target;
+  text = elementr.textContent;
+  if (flag == 1 && text != "AC") {
+    document.querySelector(".displayarea").replaceChildren();
+    document.querySelector(".displayarea").style.visibility = "visible";
+    flag = 0;
+  }
+  newdiv = document.createElement("span");
 
-document.addEventListener("click",function(event){
-    if(isequalpressed== true){
-        document.querySelector('.displayarea').replaceChildren()
-        isequalpressed=false
-    }
-    elementr = event.target
-    text = elementr.textContent
-    if(flag==1 && text!="AC"){
-        document.querySelector('.displayarea').replaceChildren()
-        document.querySelector('.displayarea').style.visibility= "visible"
-        flag=0
-    }
-    newdiv = document.createElement('span')
-    
-    
-    if(text!="="){
-        newdiv.innerHTML = text
-    }
-    // newdiv.style.margin="200px"
-    newdiv.style.textAlign= "center"
-    newdiv.style.lineHeight="2"
-    newdiv.style.fontSize="1.5rem"
-    newdiv.style.marginLeft="4px"
-    newdiv.style.paddingTop="10px"
-    newdiv.style.color="white"
-    document.querySelector('.displayarea').appendChild(newdiv)
-    if(text=="AC" && flag ==0){
-        document.querySelector('.displayarea').style.visibility= "hidden"
-        flag=1
-    }
-    if(text=="="){
-        // document.querySelector('.displayarea').replaceChildren()
-
-        // replaceChildren() can be used to clear the element
-        
-        ans= document.createElement('div')
-        ans.style.height="75px"
-        ans.style.lineHeight="3"
-        ans.style.paddingLeft="143px"
-        ans.style.fontSize="1.5rem"
-        ans.style.overflow="hidden"
-        ans.style.color="white"
-        ans.innerHTML= eval(string)
-        // console.log(eval(string))
-        ans.style.textAlign="center"
-        document.querySelector('.displayarea').appendChild(ans)
-        isequalpressed= true
-        string =""
-    }
-    if(text!="AC" && text!="="){
-        string+=text
-    }
-})
-
+  if (text != "=") {
+    newdiv.innerHTML = text;
+  }
+  newdiv.style.textAlign = "center";
+  newdiv.style.lineHeight = "2";
+  newdiv.style.fontSize = "1.5rem";
+  newdiv.style.marginLeft = "4px";
+  newdiv.style.paddingTop = "10px";
+  newdiv.style.color = "white";
+  document.querySelector(".displayarea").appendChild(newdiv);
+  if (text == "AC" && flag == 0) {
+    document.querySelector(".displayarea").style.visibility = "hidden";
+    flag = 1;
+  }
+  if (text == "=") {
+    ans = document.createElement("div");
+    ans.style.height = "75px";
+    ans.style.lineHeight = "3";
+    ans.style.paddingLeft = "143px";
+    ans.style.fontSize = "1.5rem";
+    ans.style.overflow = "hidden";
+    ans.style.color = "white";
+    ans.innerHTML = eval(string);
+    ans.style.textAlign = "center";
+    document.querySelector(".displayarea").appendChild(ans);
+    isequalpressed = true;
+    string = "";
+  }
+  if (text != "AC" && text != "=") {
+    string += text;
+  }
+});
